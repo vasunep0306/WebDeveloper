a8f994961688432207655ee763a5ee86ede14ec1
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2017-05-17 12:43:12 -0400

    finsihed javascript excersise.

---
+++ Javascript Tutorial/JavaScript stalker excersise/script.js
@@ -0,0 +1,5 @@
+var user_first_name = prompt("What is your first name");
+var user_last_name = prompt("What is your last name?");
+var user_age = prompt("What is your age?");
+alert("You are " + user_first_name + " " + user_last_name);
+alert("You are " + user_age + " years old");
---
+++ Javascript Tutorial/script.js
@@ -0,0 +1,3 @@
+alert("Hello, world");
+var user_name = prompt("What is your name");
+alert("Hello, " + user_name);

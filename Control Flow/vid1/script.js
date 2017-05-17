ad8e7ac99419979a8cca8a807e8c1e90c651c844
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2017-05-17 13:56:17 -0400

    Finished conditional excersise

---
+++ Control Flow/vid1/script.js
@@ -0,0 +1,11 @@
+var age = Number(prompt("What is your age"));
+
+if(age < 0) {
+    alert("ERROR: Age cannot be negative");
+} else if(age == 21) {
+    alert("Happy 21st birthday!!!");
+} else if(age % 2 == 1) {
+    alert("your age is odd");
+} else if(age > 0 && Math.sqrt(n) % 1 === 0) {
+    alert("perfect square");
+}
